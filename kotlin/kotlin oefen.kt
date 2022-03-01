cd7db0b16a5c4232e5ff75915c7b32ac5673d5e1
ppcxde <zmtm@tutanota.com>
2022-03-01 08:45:07 +0100

    orderning van persoonlijke files
    persoonlijke projecten worden hier geplaatst opruiming van systeem

---
+++ kotlin/kotlin oefen.kt
@@ -0,0 +1,8 @@
+fun main() {
+    //dit is een print functie met een new line
+    //print kan ook worden gebruikt alleen dan word er geen nieuwe lijn aangemaakt.
+    /* dit een test met meerder comment lines */
+val warning = "waarschuwing!"
+val warn = "u mag dit niet doen!"
+println(warning.uppercase() + warn)
+}
